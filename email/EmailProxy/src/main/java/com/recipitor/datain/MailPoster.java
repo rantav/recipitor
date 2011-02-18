8a8d50b58464a3eb2d8cfd675f4963b65add2df1
Yonatan <yonatanm@gmail.com>
2011-02-19 01:40:56 +0200

    using GAE URLfetchservice

--- email/EmailProxy/src/main/java/com/recipitor/datain/MailPoster.java
+++ email/EmailProxy/src/main/java/com/recipitor/datain/MailPoster.java
@@ -10,16 +10,22 @@
 package com.recipitor.datain;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
 
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPMethod;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
@@ -49,17 +55,17 @@ public class MailPoster implements IMailPoster {
 	@Override
 	public boolean postMail(final Mail m) throws Exception {
 		if (LGR.isDebugEnabled()) LGR.debug("posting mail [" + m.getId() + "] to frontend [" + url + "]");
-		final HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-		conn.setDoOutput(true);
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		conn.setRequestProperty("Accept-Language", "en-us,en;q=0.5");
-		conn.setRequestProperty("Accept-Encoding", "gzip,deflate");
-		conn.setRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
-		conn.setRequestProperty("Keep-Alive", "115");
-		conn.setRequestProperty("Connection", "keep-alive");
-		conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-		final DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+		final URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
+		final HTTPRequest request = new HTTPRequest(new URL(url), HTTPMethod.POST);
+		request.addHeader(new HTTPHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
+		request.addHeader(new HTTPHeader("Accept-Language", "en-us,en;q=0.5"));
+		request.addHeader(new HTTPHeader("Accept-Encoding", "gzip,deflate"));
+		request.addHeader(new HTTPHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7"));
+		request.addHeader(new HTTPHeader("Keep-Alive", "115"));
+		request.addHeader(new HTTPHeader("Connection", "keep-alive"));
+		request.addHeader(new HTTPHeader("Content-Type", "multipart/form-data; boundary=" + boundary));
+		final ByteArrayOutputStream bb = new ByteArrayOutputStream();
+		final DataOutputStream dos = new DataOutputStream(bb);
 		final InputStream is = TaskHandlerServlet.class.getClassLoader().getResourceAsStream("img.png");
 		final ByteArrayOutputStream bo = new ByteArrayOutputStream();
 		final byte[] buff = new byte[1024];
@@ -69,7 +75,7 @@ public class MailPoster implements IMailPoster {
 			bo.write(buff, 0, len);
 		}
 		is.close();
-		addPart("utf8", "V", dos);
+		//		addPart("utf8", "V", dos);
 		addPart("receipt[description]", m.getSubject(), dos);
 		addPart("user_email", m.getFrom(), dos);
 		final byte[] b = m.getAttachment() == null ? bo.toByteArray() : m.getAttachment().getBytes();
@@ -77,7 +83,8 @@ public class MailPoster implements IMailPoster {
 		addFooter(dos);
 		dos.flush();
 		dos.close();
-		final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		final HTTPResponse resp = urlFetchService.fetch(request);
+		final BufferedReader in = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(resp.getContent())));
 		String decodedString;
 		final StringBuilder sb = new StringBuilder();
 		while ((decodedString = in.readLine()) != null)
