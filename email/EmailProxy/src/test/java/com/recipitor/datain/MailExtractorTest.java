f6d84f915d60207dca6d38b8145c49c116451ed5
Yonatan <yonatanm@gmail.com>
2011-02-23 23:19:37 +0200

    refactoring

--- email/EmailProxy/src/test/java/com/recipitor/datain/MailExtractorTest.java
+++ email/EmailProxy/src/test/java/com/recipitor/datain/MailExtractorTest.java
@@ -9,12 +9,8 @@
  */
 package com.recipitor.datain;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
 import junit.framework.Assert;
 
-import org.apache.geronimo.mail.util.Base64DecoderStream;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
@@ -48,16 +44,13 @@ public class MailExtractorTest {
 
 	@Test
 	public void testBase64() throws Exception {
-		//		final Base64DecoderStream base64DecoderStream = (Base64DecoderStream) bp.getContent();
-		final Base64DecoderStream base64DecoderStream = new Base64DecoderStream(new FileInputStream("/tmp/a.64.txt"));
-		final byte[] data = MailExtractor.toByteArray(base64DecoderStream);
-		System.out.println("data length is " + data.length);
-		//		final byte[] encodeBase64 = Base64.decode(data);
-		//		final String s = new String(encodeBase64, "UTF-8");
-		final byte[] arr = data;//s.getBytes("UTF-8");
-		final FileOutputStream fos = new FileOutputStream("/tmp/b");
-		fos.write(arr);
-		fos.flush();
-		fos.close();
+		//		final Base64DecoderStream base64DecoderStream = new Base64DecoderStream(new FileInputStream("/tmp/a.64.txt"));
+		//		final byte[] data = MailExtractor.toByteArray(base64DecoderStream);
+		//		System.out.println("data length is " + data.length);
+		//		final byte[] arr = data;
+		//		final FileOutputStream fos = new FileOutputStream("/tmp/b");
+		//		fos.write(arr);
+		//		fos.flush();
+		//		fos.close();
 	}
 }
